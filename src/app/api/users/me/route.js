9deddc7e9a18e152174f0e7e887bedc8ae9634e4
Hanay <hanay.work@gmail.com>
2024-06-22 05:25:44 +0200

    added middleware and data fetching

---
+++ src/app/api/users/me/route.js
@@ -0,0 +1,18 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import { connect } from '@/dbConfig/dbConfig';
+
+export async function GET(req) {
+    await connect();
+    try {
+        const userID = getDataFromToken(req);
+        const user = await User.findOne({ _id: userID }).select("-password");
+        return NextResponse.json({ 
+            message: "User fetched successfully",
+            data: user,
+        });
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 400 });
+    }
+}
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,9 +4,12 @@ import axios from "axios";
 import Link from "next/link";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("nothing");
   const logout = async () => {
     try {
       const response = await axios.post("/api/users/logout");
@@ -19,14 +22,27 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    try {
+      const res = await axios.get("/api/users/me")
+      console.log(res.data)
+      setData(res.data.data._id)
+    } catch (error) {
+      console.log(error)
+    }
+ 
+  }
+
   return (
     <div className="grid place-items-center w-svw h-svh bg-black">
       <div className="flex flex-col justify-center items-center rounded-md shadow-[0_0_70px_rgba(255,255,255,0.5)] p-6 gap-4 bg-white">
         <h1>Profile</h1>
         <hr />
         <p>Profile page</p>
+        <h2>{data == "nothing" ? "no data" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
         <hr />
         <button onClick={logout} className="bg-red-500 text-white rounded-md p-4">Logout</button>
+        <button onClick={getUserDetails} className="bg-red-500 text-white rounded-md p-4">Get user details</button>
       </div>
     </div>
   );
