0edf985609e91c8a48e36c1c6f462cb6c1d7ebd7
X-Skoprio <61594440+X-Skoprio@users.noreply.github.com>
2024-06-30 15:29:27 +0200

    custom scrollbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="overflow-x-hidden">
+    <html lang="en" className="overflow-y-auto">
+      <body className="overflow-x-hidden ">
       <Navbar />
         {children}
       <Footer />
