a25c14b9d7ab543ede3475af910f23b3f5328cb1
X-Skoprio <61594440+X-Skoprio@users.noreply.github.com>
2024-07-05 15:23:03 +0200

    middle ware add back and added missing pages

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  const path = request.nextUrl.pathname;
+  const isPublic = path === "/login" || path === "/signup" || path === "/";
+  const token = request.cookies.get("token")?.value;
+
+  if ((path === "/login" || path === "/signup") && token) {
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
+
+  if (!isPublic && !token) {
+    return NextResponse.redirect(new URL("/login", request.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/profile", "/login", "/signup", "/verifyEmail", "/mathematiquesPhysiquesChimie","/webDesign","/electronique","/programmation","ia"],
+};
